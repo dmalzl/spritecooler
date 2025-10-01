6edfed2de1cbd98459b5af09aa2640c672a58be9
dmalzl <UaF20091!>
2025-10-01 09:09:26 +0200

    fix cluster mcool generation

--- bin/cluster_mcool.py
+++ bin/cluster_mcool.py
@@ -2,11 +2,19 @@
 
 import glob
 import os
+import re
 
 import argparse as ap
 
-from spritefridge.combine.ioutils import clustersize_from_filename
+# needs to be fixed in spritefridge in the future
+# from spritefridge.combine.ioutils import clustersize_from_filename
 from cooler import fileops
+
+
+cs_regex = re.compile('_(?P<cs>[0-9]+)_base')
+def clustersize_from_filename(filename):
+    m = cs_regex.search(filename)
+    return int(m.group('cs'))
 
 
 def parse_args():
@@ -14,7 +22,7 @@ def parse_args():
     parser.add_argument(
         '--input',
         '-i',
-        help = 'path to directory containig clustersize coolers to merge. The name of the coolers must contain the clustersize _(?P<cs>[0-9]+)_',
+        help = 'path to directory containig clustersize coolers to merge. The name of the coolers must contain the clustersize _(?P<cs>[0-9]+)_base',
         required = True
     )
     parser.add_argument(
